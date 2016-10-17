4d2550ee9ad1c97946c6779ce6c66e15397e7590
Sergio Girao <sergio.girao@touchvie.com>
2016-10-17 14:28:44 +0200

    Creating modules inherit from HorizontalListModule. Creating views for the modules.

--- SDKFrontiOS/SDKFrontiOS/Cast.swift
+++ SDKFrontiOS/SDKFrontiOS/Cast.swift
@@ -1,14 +1,14 @@
 //
-//  AppearsIn.swift
+//  Cast.swift
 //  SDKFrontiOS
 //
-//  Created by Sergio Girao on 16/10/16.
+//  Created by Sergio Girao on 17/10/16.
 //  Copyright © 2016 Tagsonomy. All rights reserved.
 //
 
 import UIKit
 
-class AppearsIn: RectangularImageItems {
+class Cast: HorizontalListModule {
 
     override func awakeFromNib() {
         super.awakeFromNib()
